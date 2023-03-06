292a8671d0e6580b9310e4b48c160137d0064fc1
unknown <achref.aguel@esprit.tn>
2023-03-06 13:29:46 +0100

    FinishPartLogin

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './auth.module.scss'
 import loginImg from '../../assets/login.jpg'
-
+import { Link } from 'react-router-dom'
+import {FaGoogle} from 'react-icons/fa'
 const login = () => {
   return (
     <section className={`container ${styles.auth }`}>
@@ -13,8 +14,17 @@ const login = () => {
             <form>
                 <input type="text" placeholder="Email" required/>
                 <input type="text" placeholder="Password" required/>
-                <button className='--btn --btn-primary'>Login</button>
+                <button className='--btn --btn-primary --btn-block'>Login</button>
+                <div className={styles.links}>
+                    <Link to='/reset'>Reset Password</Link>
+                </div>
+                <p>-- or --</p>
             </form>
+            <button className='--btn --btn-danger --btn-block'> <FaGoogle color='#fff'/>  Login With Google</button>
+            <span className={styles.register}>
+                <p>Don't have an account ?</p>
+                <Link to='/register'> Please Register</Link>
+            </span>
         </div>
         
     </section>
